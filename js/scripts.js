f5ac7a4e02ed52979074dff5d1838fac0997bcaa
Jamie Pittak and Panatda Inthala <jamie@>
2018-01-16 09:57:03 -0800

    update js function

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $('#favoriteThings').submit(function(event){
+  event.preventDefault();
+
+  var favoriteThings1 = $('#favoriteThings1').val();
+  var favoriteThings2 = $('#favoriteThings2').val();
+  var favoriteThings3 = $('#favoriteThings3').val();
+
+  var favoriteThings = [];
+
+  favoriteThings.push(favoriteThings1,favoriteThings2, favoriteThings3);
+
+
+    $(#output).text(favoriteThings.toString());
+  });
+
+});
